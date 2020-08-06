5e74da3162e5e94b44f603ec602a31c0f3fea918
nanox77 <castellano.mariano3@gmail.com>
2020-08-06 00:48:31 -0300

    Add twitter clone part 1

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - Simple list view with a favorite selection. [Post](https://medium.com/@castellano.mariano/create-a-favorite-selection-in-listview-using-flutter-fc40d3211aeb)
 - Navigate between screens. [Post](https://medium.com/@castellano.mariano/navigate-between-screens-flutter-9451be448d15)
 - Using Badges — Flutter [Post](https://medium.com/@castellano.mariano/using-badges-flutter-3cef33b18dd8)
+- Twitter UI clone using Flutter — Part 1 [Post](https://medium.com/@castellano.mariano/twitter-ui-clone-using-flutter-part-1-58602b516297)
---
+++ twitter_clone/lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Twitter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColorBrightness: Brightness.light,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: Container(
+          margin: const EdgeInsets.all(10.0),
+          child: CircleAvatar(
+            backgroundImage: AssetImage('nano.jpeg'),
+          ),
+        ),
+        title: Text(
+          'Home',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            buildBottomIconButton(Icons.home, Colors.blue),
+            buildBottomIconButton(Icons.search, Colors.black45),
+            buildBottomIconButton(Icons.notifications, Colors.black45),
+            buildBottomIconButton(Icons.mail_outline, Colors.black45),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildBottomIconButton(IconData icon, Color color) {
+    return IconButton(
+      icon: Icon(
+        icon,
+        color: color,
+      ),
+      onPressed: () {},
+    );
+  }
+}
